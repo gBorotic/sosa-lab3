ab975368d09531fe31fc1d9d894cf881a3ebabdd
Gordana Borotić <gordana.borotic@fer.hr>
2022-06-04 17:44:07 +0200

    first commit

---
+++ dodatak_A.py
@@ -0,0 +1,29 @@
+import getpass
+
+class OperationsManager():
+
+    def __init__(self, a: float, b: float) -> None:
+        self.a = a
+        self.b = b
+
+    def perform_division(self) -> float:
+        """Divides a with b. If b is zero, returns NaN."""
+        return self.a / self.bb
+
+    def toFloat(x):
+        return float(x)
+
+
+if __name__ == "__main__":
+    user = input("Username: ")
+    password = getpass.getpass("Password: ")
+    if user != "root" or password != "123":
+        print("Wrong username or password!")
+        exit(0)
+    else:
+        print("Login success!")
+        a = float(input("A = "))
+        b = float(input("B = "))
+        ops_manager = OperationsManager(a, b)
+        print(ops_manager.perform_division())
+
