9994fe76ff22ff9ba4b2e2e47aef07908f4ef450
Gordana Borotić <gordana.borotic@fer.hr>
2022-06-05 23:00:05 +0200

    dodatak_A fixed

--- dodatak_A.py
+++ dodatak_A.py
@@ -1,4 +1,6 @@
 import getpass
+from math import nan
+import bcrypt
 
 class OperationsManager():
 
@@ -8,16 +10,26 @@ class OperationsManager():
 
     def perform_division(self) -> float:
         """Divides a with b. If b is zero, returns NaN."""
+        if self.b == 0:
+            return nan
         return self.a / self.b
 
     def toFloat(x):
-        return float(x)
+        try:
+            result = float(x)
+            return result
+        except:
+            return nan
 
 
 if __name__ == "__main__":
     user = input("Username: ")
     password = getpass.getpass("Password: ")
-    if user != "root" or password != "123":
+    bytePassword = password.encode('utf-8')
+    #salt = bcrypt.gensalt()
+    salt = b'$2b$12$UxzsjL/TQ4BAhiC6U6fyku'
+    hashedPassword = b'$2b$12$UxzsjL/TQ4BAhiC6U6fykuNiV.nV0d0PrpQRe8hDkAopCn1Km7X8e'
+    if user != "root" or bcrypt.hashpw(bytePassword, salt) != hashedPassword:
         print("Wrong username or password!")
         exit(0)
     else:
